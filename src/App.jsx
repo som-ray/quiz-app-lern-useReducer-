1378e438aba4ec0d99d81ab4742da424ad451ccb
envolta-som <envoltasomnath@gmail.com>
2025-11-26 18:49:05 +0530

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import DateCounter from "./components/DateCounter";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
+}
+
+export default App;
