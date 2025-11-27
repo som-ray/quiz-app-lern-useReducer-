9618725d3cf25077d8e6970b59cdd1f772ca5005
Somnath K <karmakarsomnathy@gmail.com>
2025-11-28 00:10:42 +0530

    updating the quiz app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useReducer } from "react";
 import DateCounter from "./components/DateCounter";
 
 import Main from "./components/Main";
@@ -6,7 +6,47 @@ import Header from "./components/Header";
 
 import "./App.css";
 
+const INITIAL_STATE = {
+  question: [],
+  status: "loading"
+}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataRecived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready"
+      }
+
+    case "dataFaild":
+      return {
+        ...state,
+        status: "error"
+      }
+
+    default: throw new Error("Not valid Type")
+  }
+}
+
+
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE)
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "dataLoad", payload: data })
+        console.log(data)
+      })
+      .catch((err) => {
+        dispatch({ type: "dataFaild" })
+        console.error(err)
+      })
+  }, [])
   return (
     <div className="app">
       {/* Uncommnet the DateCounter for learning the frist part */}
